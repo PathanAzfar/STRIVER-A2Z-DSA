9dd7bba14a540e556c99e312d57d202889566273
Pathan Azfar <pattanazfar@gmail.com>
2025-07-27 01:49:17 +0530

    Initial Commit

---
+++ 3.Arrays/Easy/Intersection Of Arrays With Duplicates/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int[] a = new int[n1];
+        for (int i = 0; i < n1; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int n2 = sc.nextInt();
+        int[] b = new int[n2];
+        for (int i = 0; i < n2; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        int[] finArr = intersectionArrayWithDuplicates(a, n1, b, n2);
+        for (int i : finArr) {
+            System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+
+    private static int[] intersectionArrayWithDuplicates(int[] a, int n1, int[] b, int n2) {
+        ArrayList<Integer> arrList = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < n1 && j < n2) {
+            if (a[i] < b[j]) {
+                i++;
+            } else if (b[j] < a[i]) {
+                j++;
+            } else {
+                
+                arrList.add(a[i]);
+                i++;
+                j++;
+            }
+        }
+
+        int[] result = new int[arrList.size()];
+        for (int k = 0; k < arrList.size(); k++) {
+            result[k] = arrList.get(k);
+        }
+
+        return result;
+    }
+}
