2e32fa0b7d92b6dcc3d2b72107a87d8d2c4e45f2
Pathan Azfar <pattanazfar@gmail.com>
2025-08-19 19:57:46 +0530

    Initial Commit

---
+++ 4.Binary Search/BS on 1D Arrays/Search in Rotated Sorted Array 2/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
+		int k =sc.nextInt();
+		int ind = searchInRotatedMat(arr,n,k);
+		System.out.print(ind);
+	}
+	
+	private static int searchInRotatedMat(int[] arr,int n,int k){
+	    int low=0,high=n-1;
+	    while(low<=high){
+	        int mid = (low+high)/2;
+	        if(arr[mid]==k){
+	            return mid;
+	        }
+	    if(arr[low]==arr[mid] && arr[mid]==arr[high]){
+	        low=low+1;
+	        high=high-1;
+	        continue;
+	    }
+	            
+	            if(arr[low]<=arr[mid]){
+	                if(arr[low]<= k && k < arr[mid])
+	                 high = mid-1;
+	                 else
+	                 low=mid+1;
+	            }
+	        else{
+	            
+	          if (arr[mid] < k && k <= arr[high])
+	             low = mid+1;
+	          else
+	             high = mid-1;
+	        }
+	    }
+	    return -1;
+	}
+}
