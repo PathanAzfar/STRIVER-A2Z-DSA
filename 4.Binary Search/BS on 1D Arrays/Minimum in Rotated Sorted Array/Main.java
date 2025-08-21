47dc9e8ece03d8d755efc0eeb286781ca5bf51b6
Pathan Azfar <pattanazfar@gmail.com>
2025-08-21 15:14:36 +0530

    Initial Commit

---
+++ 4.Binary Search/BS on 1D Arrays/Minimum in Rotated Sorted Array/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
+		
+		int min = findMin(arr,n);
+		System.out.print(min);
+	}
+	
+	private static int findMin(int[] arr,int n){
+	    int low=0,high=n-1;
+	    int minEle=Integer.MAX_VALUE;
+	    while(low<=high){
+	        int mid = (low+high)/2;
+	        if(arr[low]<=arr[mid]){
+	                minEle=Math.min(minEle,arr[low]);
+	                low=mid+1;
+	           
+	        }
+	        else{
+	               minEle=Math.min(minEle,arr[mid]);
+	            high=mid-1;
+	            
+	        }
+	    }
+	    return minEle;
+	}
+}
